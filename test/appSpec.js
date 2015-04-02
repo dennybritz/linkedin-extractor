0006b23d61444e1e4b972f18bf658de1f7a0097b
Denny Britz <dennybritz@gmail.com>
2015-04-02 10:49:16 +0900

    Use nock for specs

--- test/appSpec.js
+++ test/appSpec.js
@@ -3,10 +3,19 @@
 var app = require('../app');
 var request = require('supertest-as-promised');
 var expect = require('chai').expect;
+var fs = require('fs');
+var nock = require('nock');
+
+var sampleProfileHtml = fs.readFileSync('./test/assets/studentsample.html');
 
 describe('App', function(){
 
   describe('POST /extract', function(){
+
+    nock.enableNetConnect('127.0.0.1');
+    nock('https://www.linkedin.com')
+      .get('/in/studentsample')
+      .reply(200, sampleProfileHtml);
 
     it('should work', function(){
      return request(app)
--- test/urlExtractorSpec.js
+++ test/urlExtractorSpec.js
@@ -2,15 +2,23 @@
 
 var urlExtractor = require('../lib/urlExtractor');
 var expect = require('chai').expect;
+var fs = require('fs');
+var nock = require('nock');
+
+var sampleProfileHtml = fs.readFileSync('./test/assets/studentsample.html');
 
 describe('URL Extractor', function(){
 
   it('should return a Linkedin profile from a URL', function(){
 
+    nock('https://www.linkedin.com')
+      .get('/in/studentsample')
+      .reply(200, sampleProfileHtml)
+
     return urlExtractor
-      .getFromUrlAsync('https://www.linkedin.com/in/dennybritz')
+      .getFromUrlAsync('https://www.linkedin.com/in/studentsample')
       .then(function(res){
-        expect(res.formattedName).to.eql('Denny Britz');
+        expect(res.formattedName).to.eql('Student Sample');
       });
 
   });
